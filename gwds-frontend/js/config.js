ec23abac36b51e640e857009b3466abe06274464
dcumbane <dcumbane@estudantes.unisced.edu.mz>
2026-05-22 14:40:20 +0200

    Update config.js

--- gwds-frontend/js/config.js
+++ gwds-frontend/js/config.js
@@ -4,7 +4,7 @@
  */
 export const CONFIG = {
   // EX: https://script.google.com/macros/s/AKfycb.../exec
-  API_BASE: 'COLOCAR_AQUI_O_URL_DA_WEB_APP',
+  API_BASE: 'https://script.google.com/macros/s/AKfycbycrwc3BGy0FFo0i0mWbA9pc-KIjf_uG3NS7T-YzFW35ghDY9akvj0ZlbSpTDBaMp1X/exec',
   MAP_CENTER: [-24.5, 33.2],
   MAP_ZOOM: 8,
   ORGANIZATION: 'Direcção Provincial de Obras Públicas, Habitação e Recursos Hídricos de Gaza'
